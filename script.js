79f556bbe84a269d2a3e040fdc4da768832ac8bb
Alan-oliveir <alandeog@poli.ufrj.br>
2023-03-16 16:55:50 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 const songName = document.getElementById('song-name');
+const song = document.getElementById('audio');
+const play = document.getElementById('play');
 
 songName.innerText = 'As You Were';
+let isPlaying = false;
+
+function playSong() {
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+    song.play();
+    isPlaying = true;
+}
+
+function pauseSong() {
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider() {
+    if(isPlaying === true) {
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+play.addEventListener('click', playPauseDecider);
