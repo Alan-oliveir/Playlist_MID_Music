fb59a8b9ab4ab05ec5a871f48f2089a70d60279a
Alan-oliveir <alandeog@poli.ufrj.br>
2023-03-16 17:54:47 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
 const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
 const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
 
-songName.innerText = 'As You Were';
+const asYouWere = {
+    songName: 'As You Were',
+    artist: 'Track Tribe',
+    file: 'as_you_were'
+};
+
+const boomBapFlick = {
+    songName: 'Boom Bap Flick',
+    artist: 'Quincas Moreira',
+    file: 'boom_bap_flick'
+};
+
+const cantHide = {
+    songName: "Can't Hide",
+    artist: 'Otis McDonald',
+    file: 'cant_hide'
+};
+
 let isPlaying = false;
+const playlist = [asYouWere, boomBapFlick, cantHide];
+let index = 0;
 
 function playSong() {
     play.querySelector('.bi').classList.remove('bi-play-circle-fill');
@@ -28,4 +51,37 @@ function playPauseDecider() {
     }
 }
 
+function initializeSong() {
+    cover.src = `images/${playlist[index].file}.webp`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName; 
+    bandName.innerText = playlist[index].artist;
+}
+
+function previousSong() {
+    if(index === 0) {
+        index = playlist.length - 1;
+    }
+    else {
+        index -= 1;
+    }
+    initializeSong();
+    playSong();   
+}
+
+function nextSong() {
+    if(index === playlist.length - 1) {
+        index = 0;
+    }
+    else {
+        index += 1;
+    }
+    initializeSong();
+    playSong();   
+}
+
+initializeSong();
+
 play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
